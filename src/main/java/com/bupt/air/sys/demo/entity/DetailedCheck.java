b17cb544a2e20ea7c1a62cb564bbd39da69425e8
wswjjjjwsw <wjj289764820@icloud.com>
2020-06-25 22:12:17 +0800

    check and detailed check entity

---
+++ src/main/java/com/bupt/air/sys/demo/entity/DetailedCheck.java
@@ -0,0 +1,19 @@
+package com.bupt.air.sys.demo.entity;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@ApiModel(value = "Room-Record", description = "房间记录示例")
+public class DetailedCheck {
+    private int roomid;
+    private Timestamp startTime;
+    private Timestamp endTime;
+    boolean isPrinted;
+    private float Fee;
+
+
+}
