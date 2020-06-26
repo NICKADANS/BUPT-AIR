79d0b68f01eac2e104deb6af3835fb57110fe297
wswjjjjwsw <wjj289764820@icloud.com>
2020-06-26 23:27:00 +0800

    front_controller backup before bedTime at 23:26

--- src/main/java/com/bupt/air/sys/demo/service/FrontService.java
+++ src/main/java/com/bupt/air/sys/demo/service/FrontService.java
@@ -2,22 +2,43 @@ package com.bupt.air.sys.demo.service;
 
 import com.bupt.air.sys.demo.entity.Check;
 import com.bupt.air.sys.demo.entity.DetailedCheck;
+import com.bupt.air.sys.demo.entity.Room;
+import com.bupt.air.sys.demo.repository.RecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @Service
 public class FrontService {
-    public Check Checkout(int roomid){
+    @Autowired
+    RecordRepository recordRepository;
+    @Autowired
+    CentralAC centralAC;
+
+    public Check Checkout(int roomid,Timestamp endTime){
+        List<Room> rooms = centralAC.getRooms();
+        Room room = rooms.get(roomid);
         Check ch = new Check();
+        ch.setRoomid(roomid);
+        Timestamp startTime = room.getT_checkin();
+        ch.setEndTime(startTime);
+        ch.setStartTime(endTime);
+        ch.setPrinted(true);
+        ch.setFee(room.getFee());
         return ch;
     }
 
     public DetailedCheck detailedRequest(int roomid,Timestamp startTime,Timestamp endTime){
         DetailedCheck dch = new DetailedCheck();
+        dch.setRoomid(roomid);
         dch.setEndTime(endTime);
+        dch.setStartTime(startTime);
         dch.setPrinted(true);
-
+        dch.setRecords(recordRepository.findByOpttimeBetween(startTime,endTime));
+        int size = dch.getRecords().size();
+        dch.setFee(dch.getRecords().get(size).getFee());
         return dch;
 
     }
