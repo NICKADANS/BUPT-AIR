88f3da796a3974f17dc96cbc5c75242e82e78d63
wswjjjjwsw <wjj289764820@icloud.com>
2020-06-26 20:58:34 +0800

    front_controller backup at 9pm //changed return form into "Result"

--- src/main/java/com/bupt/air/sys/demo/controller/FrontController.java
+++ src/main/java/com/bupt/air/sys/demo/controller/FrontController.java
@@ -5,6 +5,7 @@ import com.bupt.air.sys.demo.entity.DetailedCheck;
 import com.bupt.air.sys.demo.entity.Room;
 import com.bupt.air.sys.demo.service.CentralAC;
 import com.bupt.air.sys.demo.service.FrontService;
+import com.bupt.air.sys.demo.utils.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,41 +29,41 @@ public class FrontController {
 
     @ApiOperation(value = "办理入住", notes = "检验房间是否已经办理退房，退房了初始化房间并返回房间信息，否则返回BAD_REQUEST")
     @GetMapping(path = "/CheckIn/{roomid}", produces = "application/json")
-    public ResponseEntity<Room> PrintAllRoomInfo(@PathVariable int roomid){
+    public Result<?> PrintAllRoomInfo(@PathVariable int roomid){
         Timestamp present = new Timestamp(System.currentTimeMillis());
         List<Room> rooms = centralAC.getRooms();
         Room room = rooms.get(roomid);
         if(!room.getOccupied()){
             room.CheckIn(present);
-            return  new ResponseEntity<Room>(room,HttpStatus.OK);
+            return  Result.ok(room);
         }
         else{
-            return  new ResponseEntity<Room>(HttpStatus.BAD_REQUEST);
+            return  Result.error(400,"room is  already Occupied");
         }
     }
 
     @ApiOperation(value = "办理退房", notes = "如果已经退房返回BAD_REQUEST，未退房返回一个Check")
     @PostMapping(path = "/CheckOut/{roomid}",consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Check> CheckOut(@PathVariable int roomid){
+    public Result<?>  CheckOut(@PathVariable int roomid){
         List<Room> rooms = centralAC.getRooms();
         Room room = rooms.get(roomid);
         if(room.getOccupied()){
 
             room.CheckOut();
             Check ch = service.Checkout();
-            return  new ResponseEntity<Check>(ch,HttpStatus.BAD_REQUEST);
+            return  Result.ok(ch);
         }
         else{
-            return  new ResponseEntity<Check>(HttpStatus.BAD_REQUEST);
+            return  Result.error(400,"room is  already checked out");
         }
 
     }
 
     @ApiOperation(value = "办理退房", notes = "如果已经退房返回BAD_REQUEST，未退房返回一个Check")
     @PostMapping(path = "/Detailed/{roomid}/{startTime}/{endTime}",consumes = "application/json", produces = "application/json")
-    public ResponseEntity<DetailedCheck> CheckOut(@PathVariable int roomid,@PathVariable Timestamp startTime,@PathVariable Timestamp endTime){
-        service.detailedRequest();
-        return new ResponseEntity<Check>(,HttpStatus.OK);
+    public Result<?>  CheckOut(@PathVariable int roomid,@PathVariable Timestamp startTime,@PathVariable Timestamp endTime){
+        DetailedCheck dch = service.detailedRequest(roomid,startTime,endTime);
+        return  Result.ok(dch);
 
     }
 
